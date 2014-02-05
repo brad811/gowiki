6c82a6491b5706f8710b607d5712468cb75fdb38
Brad Jewell <brad811@gmail.com>
2014-02-05 15:10:00 -0600

    spruced up html files, added css and static assets serving

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -94,5 +94,8 @@ func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	http.Handle("/static/", http.FileServer(http.Dir("./")))
+
 	http.ListenAndServe(":8080", nil)
 }
