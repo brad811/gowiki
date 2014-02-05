32c4b9eaa4c3010524742c96f4a26bba7240ac78
Brad Jewell <brad811@gmail.com>
2014-02-05 14:37:10 -0600

    made root directory redirect to /view/FrontPage

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -85,7 +85,12 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+}
+
 func main() {
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
